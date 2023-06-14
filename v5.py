126fa3e1681bdd928514b9ac82615bb2fb78c119
Erhan <ep4963807@gmail.com>
2023-06-14 19:41:24 +0300

    update

--- v5.py
+++ v5.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from threading import Thread
 import time
 
-def play_random_sound(folder_path):
+def play_random_sound(folder_path, play_time):
     sound_files = os.listdir(folder_path)
     sound_file = random.choice(sound_files)
     sound_path = os.path.join(folder_path, sound_file)
@@ -20,14 +20,15 @@ def play_random_sound(folder_path):
 
 def start_playing():
     folder_path = folder_path_entry.get()
+    play_time = int(play_time_entry.get()) * 1000  # Convert seconds to milliseconds
     status_label.config(text="Program Çalışıyor")
-    play_thread = Thread(target=continuous_play, args=(folder_path,))
+    play_thread = Thread(target=continuous_play, args=(folder_path, play_time))
     play_thread.start()
 
-def continuous_play(folder_path):
+def continuous_play(folder_path, play_time):
     while True:
-        play_random_sound(folder_path)
-        time.sleep(random.randint(10, 60))
+        play_random_sound(folder_path, play_time)
+        time.sleep(random.randint(min_wait_time, max_wait_time))
 
 def stop_playing():
     status_label.config(text="Program Çalışmıyor")
@@ -43,6 +44,12 @@ folder_path_label = tk.Label(window, text="Klasör Yolu:")
 folder_path_label.pack(pady=10)
 folder_path_entry = tk.Entry(window)
 folder_path_entry.pack(pady=5)
+
+# Oynama süresi metin kutusu
+play_time_label = tk.Label(window, text="Oynama Süresi (saniye):")
+play_time_label.pack(pady=10)
+play_time_entry = tk.Entry(window)
+play_time_entry.pack(pady=5)
 
 # Başlat düğmesini oluşturma
 start_button = tk.Button(window, text="Başlat", command=start_playing)
